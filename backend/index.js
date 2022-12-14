38e65952f8c018c0313aa9cfa77ef69453d49b86
valentina vittoria <valeviit@icloud.com>
2022-12-14 23:55:51 +0100

    connect the database

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 import express,{json} from "express";
 import workoutRoutes from "./routes/workouts.js"
 import {} from 'dotenv/config';
+import mongoose from "mongoose";
 // express app
 const app = express();
 
 //import PORT AND URI
 const PORT = process.env.PORT;
-const DB_URI= process.env.DB_URI;
+const MONGO_URI= process.env.MONGO_URI;
 
 //middlewear
 app.use(json());
@@ -19,7 +20,14 @@ app.use((req,res,next)=>{
 //Routes
 app.use("/api/workouts", workoutRoutes)
 
-//listen
+//Connect db
+mongoose.connect(MONGO_URI)
+.then(()=>{
+    //listen
 app.listen(PORT,()=>{
-    console.log(`listening on port ${PORT}`)
+    console.log(`connected to db and listening on port ${PORT}`)
+})
+})
+.catch((error)=>{
+    console.log(error)
 })
